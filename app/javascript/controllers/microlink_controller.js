49a956851885e845cd96cd2d0a3eebf218077a71
Steve Polito <info@stevepolitodesign.com>
2021-02-21 11:06:43 -0500

    Create base Stimulus Controller.

--- app/javascript/controllers/microlink_controller.js
+++ app/javascript/controllers/microlink_controller.js
@@ -2,5 +2,10 @@ import { Controller } from "stimulus"
 import mql from "@microlink/mql"
 
 export default class extends Controller {
+    static targets = [ "input" ]
 
+    async handleChange() {
+        const { status, data } = await mql(this.inputTarget.value)
+        console.log(status, data)
+    }
 }
