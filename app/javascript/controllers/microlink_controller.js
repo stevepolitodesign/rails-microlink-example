7e36190061c0a8a62f6dcc08ffc78dc6c6ec46d5
Steve Polito <info@stevepolitodesign.com>
2021-02-22 09:05:24 -0500

    Render preview.

--- app/javascript/controllers/microlink_controller.js
+++ app/javascript/controllers/microlink_controller.js
@@ -2,18 +2,32 @@ import { Controller } from "stimulus"
 import mql from "@microlink/mql"
 
 export default class extends Controller {
-    static targets = [ "input", "descriptionInput", "imageInput", "titleInput" ]
+    static targets = [ "input", "descriptionInput", "imageInput", "titleInput", "output" ]
+
+    connect() {
+        this.previewDescription = this.outputTarget.querySelector("p");
+        this.previewImage       = this.outputTarget.querySelector("img");
+        this.previewTitle       = this.outputTarget.querySelector("h5");
+    }
 
     async handleChange() {
         const { status, data } = await mql(this.inputTarget.value)
         if(status == "success") {
             this.setFormData(data);
+            this.renderPreview(data);
         }
     }
 
+    renderPreview(data) {
+        this.previewDescription.innerHTML = data?.description ? data.description : null;
+        data?.image?.url ? this.previewImage.setAttribute("src", data.image.url) : null;
+        this.previewTitle.innerHTML = data?.title ? data.title : null;
+        this.outputTarget.style.display = "block";
+    }
+
     setFormData(data) {
-        this.descriptionInputTarget.value   = data?.description ? data?.description : null;
-        this.imageInputTarget.value         = data?.image?.url ? data?.image?.url : null;
-        this.titleInputTarget.value         = data?.title ? data?.title : null;
+        this.descriptionInputTarget.value   = data?.description ? data.description : null;
+        this.imageInputTarget.value         = data?.image?.url ? data.image.url : null;
+        this.titleInputTarget.value         = data?.title ? data.title : null;
     }
 }
