07b59f3aa8bce50691fc3892d94fff6728e8e5e9
Steve Polito <info@stevepolitodesign.com>
2021-02-23 07:52:27 -0500

    Build job to attach thumbnail to link.

--- app/jobs/microlink_image_attacher_job.rb
+++ app/jobs/microlink_image_attacher_job.rb
@@ -1,7 +1,12 @@
+require "down"
+
 class MicrolinkImageAttacherJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(link)
+    if link.image.present?
+      tempfile = Down.download(link.image)
+      link.thumbnail.attach(io: tempfile, filename: tempfile.original_filename) 
+    end
   end
 end
