86246b22d9ec208ab03f488ec294d383d0f4ee24
Steve Polito <info@stevepolitodesign.com>
2021-02-23 07:51:42 -0500

    Add has_one_attached :thumbnail to Link Model.

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ApplicationRecord
     # https://api.rubyonrails.org/classes/ActiveRecord/Store.html
     store :meta_data, accessors: [ :description, :image, :title  ], coder: JSON
     validates :url, presence: true
+    has_one_attached :thumbnail
 end
